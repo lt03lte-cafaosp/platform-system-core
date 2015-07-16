2ecf7ebdad918c025e96616113f92e2749dbac01
Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
2015-07-16 12:22:52 -0700

    diag: Add new qcom_diag group to access /dev/diag
    This change adds the definition of qcom_diag and
    also modify the /dev/diag node group to qcom_diag.
    
    Change-Id: I07bd099c76f3fd2685f3a6698e37314a0a2c8f44

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -97,6 +97,7 @@
 #define AID_SHELL         2000  /* adb and debug shell user */
 #define AID_CACHE         2001  /* cache access */
 #define AID_DIAG          2002  /* access to diagnostic resources */
+#define AID_QCOM_DIAG     2009  /* can read/write /dev/diag */
 
 /* The range 2900-2999 is reserved for OEM, and must never be
  * used here */
@@ -207,6 +208,7 @@ static const struct android_id_info android_ids[] = {
     { "shell",         AID_SHELL, },
     { "cache",         AID_CACHE, },
     { "diag",          AID_DIAG, },
+    { "qcom_diag",     AID_QCOM_DIAG, },
 
     { "net_bt_admin",  AID_NET_BT_ADMIN, },
     { "net_bt",        AID_NET_BT, },
