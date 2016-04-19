bf5b79bd8b6f49981a9b8acaf87e75119f2354a1
Elliott Hughes <enh@google.com>
2016-04-19 14:57:09 -0700

    Fix scanf %s in lsof.
    Conflicts:
    	toolbox/lsof.c
    
    Bug: http://b/28175237
    Change-Id: Ief0ba299b09693ad9afc0e3d17a8f664c2fbb8c2
    (cherry picked from commit ae18eb014609948a40e22192b87b10efc680daa7)

--- toolbox/lsof.c
+++ toolbox/lsof.c
@@ -99,7 +99,7 @@ void print_maps(struct pid_info_t* info)
     int major, minor;
     char device[10];
     long int inode;
-    char file[PATH_MAX];
+    char file[1024];
 
     strncat(info->path, "maps", sizeof(info->path));
 
@@ -107,8 +107,8 @@ void print_maps(struct pid_info_t* info)
     if (!maps)
         goto out;
 
-    while (fscanf(maps, "%*x-%*x %*s %zx %5s %ld %s\n", &offset, device, &inode,
-            file) == 4) {
+    while (fscanf(maps, "%*x-%*x %*s %zx %5s %ld %1023s\n",
+                  &offset, device, &inode, file) == 4) {
         // We don't care about non-file maps
         if (inode == 0 || !strcmp(device, "00:00"))
             continue;
